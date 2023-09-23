cda2deb0aa1e066baf619820075e0bb257814f9f
veeramani <stockmaveric@gmail.com>
2023-09-22 20:00:10 -0400

    removed packages

--- MainPage.py
+++ MainPage.py
@@ -6,7 +6,6 @@ Created on Fri Sep 15 18:11:20 2023
 @author: saravananveeramani
 """
 import streamlit as st
-from streamlit_extras.add_vertical_space import add_vertical_space
 import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
@@ -18,7 +17,7 @@ st.set_page_config(page_title="Fundamental Analysis App", layout="wide")
 
 st.text_input("Stock name", key="ticker",value="AAPL")
 
-   
+
 st.header("{}".format(lib.getStockName(st.session_state.ticker)))
 
 # Row #1
@@ -39,12 +38,12 @@ with row3_1:
     st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
     st.markdown(lib.dividend_desc)
-    
+
 with row3_2:
     st.subheader("EPS ")
     df = lib.getTicker(st.session_state.ticker)
     result_df = lib.formatIncomeStmtData(df.income_stmt)
-    
+
     fig = px.bar(
                 result_df,
                 x="year",
@@ -54,11 +53,11 @@ with row3_2:
                 color_discrete_sequence=["#c681eb"],
             )
     st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-        
+
     st.markdown(lib.eps_desc)
 
-# Row # 2    
-add_vertical_space()
+# Row # 2
+
 
 row4_space1, row4_1, row4_space2, row4_2, row4_space3 = st.columns(
     (0.1, 1, 0.1, 1, 0.1)
@@ -69,7 +68,7 @@ with row4_1:
     st.subheader("Revenue")
     df = lib.getTicker(st.session_state.ticker)
     result_df = lib.formatIncomeStmtData(df.income_stmt)
-    
+
     fig = px.bar(
                 result_df,
                 x="year",
@@ -80,12 +79,12 @@ with row4_1:
             )
     st.plotly_chart(fig, theme="streamlit", use_container_width=True)
     st.markdown("Yearly Income statement")
-    
+
 with row4_2:
     st.subheader("Net Income")
     df = lib.getTicker(st.session_state.ticker)
     result_df = lib.formatIncomeStmtData(df.income_stmt)
-    
+
     fig = px.bar(
                 result_df,
                 x="year",
