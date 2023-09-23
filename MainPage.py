3d86af35bf36fe0b1c0d96fd7446a10bd70a986c
veeramani <stockmaveric@gmail.com>
2023-09-22 22:16:49 -0400

    modified the files

--- MainPage.py
+++ MainPage.py
@@ -9,13 +9,38 @@ import streamlit as st
 import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
-import Dividends as lib
-from Dividends import getStockName
-from Dividends import getDividends
-from Dividends import getTicker
-from Dividends import formatIncomeStmtData
-from Dividends import eps_desc
-from Dividends import dividend_desc
+import yfinance as yf
+
+
+# Functions Starts
+
+dividend_desc = "Dividends are a portion of a company's profits that it distributes to its shareholders. They are typically paid out regularly, usually on a quarterly basis, and are a way for investors to receive a share of the company's earnings as cash payments"
+eps_desc = "Earnings Per Share (EPS) is a measure of a company's profitability that tells you how much profit it has generated for each outstanding share of its stock. It's calculated by dividing the company's total earnings by the number of shares available to the public. EPS helps investors gauge a company's financial performance and is a key factor in evaluating its stock's value and potential for growth"
+
+
+# Get dividends data
+def getDividends(ticker):
+    tkr = yf.Ticker(ticker)
+    div_data = tkr.dividends.to_frame()
+    div_data['year'] = div_data.index
+    return div_data.tail(40)
+
+# Get stock data
+def getTicker(ticker):
+    tkr = yf.Ticker(ticker)
+    return tkr
+
+def getStockName(ticker):
+    tkr = yf.Ticker(ticker)
+    return tkr.info['longName']
+
+# format income statement parameter dataframe.
+def formatIncomeStmtData(df):
+    result_df = df.T
+    result_df['year'] = result_df.index
+    return result_df
+
+# Functions End
 
 
 st.set_page_config(page_title="Fundamental Analysis App", layout="wide")
