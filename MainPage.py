15937945792ad462d0e30e872ec0b5fbe826c369
veeramani <stockmaveric@gmail.com>
2023-09-25 21:34:47 -0400

    Test

--- MainPage.py
+++ MainPage.py
@@ -15,7 +15,7 @@ import pandas as pd
 
 # Functions Starts
 
-dividend_desc = "Dividends are a portion of a company's profits that it distributes to its shareholders. They are typically paid out regularly, usually on a quarterly basis, and are a way for investors to receive a share of the company's earnings as cash payments"
+DIVIDEND_DESC = "Dividends are a portion of a company's profits that it distributes to its shareholders. They are typically paid out regularly, usually on a quarterly basis, and are a way for investors to receive a share of the company's earnings as cash payments"
 eps_desc = "Earnings Per Share (EPS) is a measure of a company's profitability that tells you how much profit it has generated for each outstanding share of its stock. It's calculated by dividing the company's total earnings by the number of shares available to the public. EPS helps investors gauge a company's financial performance and is a key factor in evaluating its stock's value and potential for growth"
 
 
